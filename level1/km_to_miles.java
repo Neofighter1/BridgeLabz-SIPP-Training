9f5a4c597abf6efc7b2747f1dad051752d3ac635
Satyam Gupta <Satyamgupta9012@gmail.com>
2025-06-21 21:15:00 +0530

    Clean Main

--- level1/km_to_miles.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-class conversion{
-	public static void main(String args[]){
-		Scanner input = new Scanner(System.in);
-		double km=input.nextInt();
-		System.out.printf("The total miles is %.2f mile for the given %.2f km",km*1.6,km);
-	}
-}
