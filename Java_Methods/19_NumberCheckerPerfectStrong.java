8280d5b524a079557613bf73ee7340bd2109b654
Neofighter1 <satyamgupta9012@gmail.com>
2025-06-22 11:30:00 +0530

    [Satyam Gupta]add.Completed_Java_Methods_Assignment

---
+++ Java_Methods/19_NumberCheckerPerfectStrong.java
@@ -0,0 +1,66 @@
+
+public class NumberCheckerPerfectStrong {
+
+    
+    public static int sumOfProperDivisors(int number) {
+        int sum = 1; 
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    
+    public static boolean isPerfectNumber(int number) {
+        if (number <= 1) return false;
+        return sumOfProperDivisors(number) == number;
+    }
+
+    
+    public static boolean isAbundantNumber(int number) {
+        if (number <= 1) return false;
+        return sumOfProperDivisors(number) > number;
+    }
+
+    
+    public static boolean isDeficientNumber(int number) {
+        if (number <= 1) return false;
+        return sumOfProperDivisors(number) < number;
+    }
+
+    
+    public static int factorial(int digit) {
+        int fact = 1;
+        for (int i = 2; i <= digit; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    
+    public static boolean isStrongNumber(int number) {
+        int sum = 0;
+        int temp = number;
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += factorial(digit);
+            temp /= 10;
+        }
+        return sum == number;
+    }
+
+    public static void main(String[] args) {
+        int[] testNumbers = {6, 12, 15, 145};
+
+        for (int number : testNumbers) {
+            System.out.println("Number: " + number);
+            System.out.println("Is perfect number? " + isPerfectNumber(number));
+            System.out.println("Is abundant number? " + isAbundantNumber(number));
+            System.out.println("Is deficient number? " + isDeficientNumber(number));
+            System.out.println("Is strong number? " + isStrongNumber(number));
+            System.out.println();
+        }
+    }
+}
