d098c6ba4969f611a308acbf7d9d84a53dc76b04
Satyam Gupta <122156091+Neofighter1@users.noreply.github.com>
2025-07-07 10:50:29 +0530

    Delete level1 directory

--- level1/distanceinmiles.java
+++
@@ -1,7 +0,0 @@
-class distanceinmiles{
-	public static void main(String [] args){
-		double d=10.8;
-		double miles=d*1.6;
-		System.out.printf("The distance %.1f km in miles is %.2f ",d,miles);
-	}
-}
